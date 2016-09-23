392aff71ab7bf9f89fcc7edf9dcc111624d6a29b
Kiren Srinivasan <email": null>
2016-09-23 10:30:09 -0400

    Starts setting up landing page-related components

---
+++ app/assets/javascripts/components/App.es6.jsx
@@ -0,0 +1,19 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current_user: this.props.user
+    };
+    
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
