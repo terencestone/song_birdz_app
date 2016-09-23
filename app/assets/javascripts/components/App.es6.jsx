e552fbb2390863b293a8c24e4b5cbeadc1768b60
Kiren Srinivasan <email": null>
2016-09-23 11:17:55 -0400

    Adds hardcoding so far for: App and Landing

--- app/assets/javascripts/components/App.es6.jsx
+++ app/assets/javascripts/components/App.es6.jsx
@@ -2,18 +2,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      current_user: this.props.user
+      current_user: this.props.current_user
     };
-
+    this.renderLandingOrMatches = this.renderLandingOrMatches.bind(this)
   }
 
   componentDidMount() {
-    
+
+  }
+
+  renderLandingOrMatches() {
+    let availableSpace;
+    // if (this.state.current_user is already signed up with omniauth and has filled out the profile edit form and gone through onboarding) {
+    //   availableSpace = <Matches />
+    // } else if (this.state.current_user is being configured for the very first time and has NOT filled out the profile edit form and gone through onboarding) {
+    //   availableSpace = <Landing current_user={this.state.current_user} />
+    // } else {
+    //   availableSpace = <Landing />
+    // }
+    return availableSpace;
   }
 
   render() {
     return(
-
+      {this.renderLandingOrMatches()}
     )
   }
 }
