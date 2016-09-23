f51575551694bfbbf25cbab01322653336d844e3
Terence Stone <email": null>
2016-09-23 19:23:41 -0400

    Uncomment component

--- app/assets/javascripts/components/Landing.es6.jsx
+++ app/assets/javascripts/components/Landing.es6.jsx
@@ -1,42 +1,42 @@
 class Landing extends React.Component {
-  constructor(props) {
-    super(props);
-    this.renderProfileEditForm = this.renderProfileEditForm.bind(this)
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.renderProfileEditForm = this.renderProfileEditForm.bind(this)
+  // }
 
 
-  componentDidMount() {
+  // componentDidMount() {
 
-  }
+  // }
 
-  renderProfileEditForm() {
-    // let availableSpace;
-    // If the "this.props.current_user" has JUST clicked the sign up button and has not configured their personal details yet on the ProfileEditForm
-    // availableSpace = <ProfileEditForm current_user={this.props.current_user} onboardingContainer/>
-    //
-    // else
-    // availableSpace = null
-    // return availableSpace;
-  }
+  // renderProfileEditForm() {
+  //   // let availableSpace;
+  //   // If the "this.props.current_user" has JUST clicked the sign up button and has not configured their personal details yet on the ProfileEditForm
+  //   // availableSpace = <ProfileEditForm current_user={this.props.current_user} onboardingContainer/>
+  //   //
+  //   // else
+  //   // availableSpace = null
+  //   // return availableSpace;
+  // }
 
 
-  render() {
-    return(
-      // Return this only if there is NOT a "this.props.current_user"
-      // <div className="unique-value-prop">
-      //   <p>
-      //     Discover romance through shared musical passion
-      //   </p>
-      // </div>
-      // <Button signUp />
-      // <Button logIn />
+  // render() {
+  //   return(
+  //     Return this only if there is NOT a "this.props.current_user"
+  //     <div className="unique-value-prop">
+  //       <p>
+  //         Discover romance through shared musical passion
+  //       </p>
+  //     </div>
+  //     <Button signUp />
+  //     <Button logIn />
 
-      //
 
-      // Return this only if there IS a "this.props.current_user"
-      // {this.renderProfileEditForm()}
-    )
-  }
+
+  //     Return this only if there IS a "this.props.current_user"
+  //     {this.renderProfileEditForm()}
+  //   )
+  // }
 
 
 }
