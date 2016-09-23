6b7d97076f6012d0e6c0bd69052f2314bef20534
Kiren Srinivasan <email": null>
2016-09-23 17:09:58 -0400

    Adds first draft for first onboarding card

--- app/assets/javascripts/components/PlaylistInfo.es6.jsx
+++ app/assets/javascripts/components/PlaylistInfo.es6.jsx
@@ -3,7 +3,7 @@ class PlaylistInfo extends React.Component {
     return(
       <div>
         <h3>Welcome to Songbirdz!</h3>
-        <p>At Songbirdz, you can create a custom “Birdlist” playlist that will be used to match you with potential partners who best share your music taste. You as a person are constantly evolving - bring your Birdlist along for the ride. You will be able to change the songs of this playlist as often as you want, either on your user profile or directly from Spotify itself.</p>
+        <p>At Songbirdz, you can create a custom “Birdlist” playlist that will be used to represent who you are and match you with potential partners who best share your tastes in music. Since you are constantly evolving as a person, you can bring your Birdlist along for the ride. You will be able to change the songs of this playlist as often as you want, either on your user profile or directly from Spotify itself.</p>
       </div>
     )
   }
