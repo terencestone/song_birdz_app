92378c013d5db99f6d8ab5ad8a2f86ef783b67be
Kiren Srinivasan <email": null>
2016-09-23 11:46:10 -0400

    Adds initial form for ProfileEditForm

--- app/assets/javascripts/components/ProfileEditForm.es6.jsx
+++ app/assets/javascripts/components/ProfileEditForm.es6.jsx
@@ -3,9 +3,42 @@ class ProfileEditForm extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+
+  }
+
   render() {
     return(
+      <div className="profile-edit-form">
+        <h3>Create your profile</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label for="age-range">Age Range</label>
+          <div className="age-range">
+            <label for="min-age">Min</label>
+            <input name="min-age" type="number" min="18" max="99" />
 
+            <span>to</span>
+
+            <label for="max-age">max</label>
+            <input name="max-age" type="number" min="19" max="100" />
+          </div>
+
+          <label for="gender">Gender</label>
+          <select name="gender">
+            {/* Determine all of the genders that we'll be adding here */}
+            <option value="each gender we want">Some gender</option>
+          </select>
+
+          <legend>Preferences</legend>
+          {/* Determine all of the "looking fors" that we'll be adding here */}
+          <input type="checkbox" name="men" value="Men" />Men
+
+          <label for="about-me">About Me</label>
+          <textarea name="about-me"></textarea>
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     )
   }
 }
