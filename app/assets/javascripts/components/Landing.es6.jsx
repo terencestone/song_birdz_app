e984e19f08dcab8e06f93fd1f6d6245851f398f3
Terence Stone <email": null>
2016-09-23 17:40:23 -0400

    Fix second error

--- app/assets/javascripts/components/Landing.es6.jsx
+++ app/assets/javascripts/components/Landing.es6.jsx
@@ -1,65 +1,42 @@
-// class Landing extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.renderProfileEditForm = this.renderProfileEditForm.bind(this)
-//   }
-<<<<<<< 0be1cb319e0478416a99d25d025e5651aba168b7
-//
-//
-//   componentDidMount() {
-//
-//   }
-//
-//   renderProfileEditForm() {
-//     // let availableSpace;
-//     // If the "this.props.current_user" has JUST clicked the sign up button and has not configured their personal details yet on the ProfileEditForm
-//     // availableSpace = <ProfileEditForm current_user={this.props.current_user} onboardingContainer/>
-//     //
-//     // else
-//     // availableSpace = null
-//     // return availableSpace;
-//   }
-//
-=======
+class Landing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderProfileEditForm = this.renderProfileEditForm.bind(this)
+  }
 
 
-//   componentDidMount() {
+  componentDidMount() {
 
-//   }
+  }
 
-//   renderProfileEditForm() {
-//     let availableSpace;
-//     // If the "this.props.current_user" has JUST clicked the sign up button and has not configured their personal details yet on the ProfileEditForm
-//     // availableSpace = <ProfileEditForm current_user={this.props.current_user} onboardingContainer/>
+  renderProfileEditForm() {
+    // let availableSpace;
+    // If the "this.props.current_user" has JUST clicked the sign up button and has not configured their personal details yet on the ProfileEditForm
+    // availableSpace = <ProfileEditForm current_user={this.props.current_user} onboardingContainer/>
+    //
+    // else
+    // availableSpace = null
+    // return availableSpace;
+  }
 
-//     // else
-//     // availableSpace = null
-//     return availableSpace;
-//   }
 
->>>>>>> Add user controller, and css
-//   render() {
-//     return(
-//       // Return this only if there is NOT a "this.props.current_user"
-//       // <div className="unique-value-prop">
-//       //   <p>
-//       //     Discover romance through shared musical passion
-//       //   </p>
-//       // </div>
-//       // <Button signUp />
-//       // <Button logIn />
-<<<<<<< 0be1cb319e0478416a99d25d025e5651aba168b7
-//       //
-=======
+  render() {
+    return(
+      // Return this only if there is NOT a "this.props.current_user"
+      // <div className="unique-value-prop">
+      //   <p>
+      //     Discover romance through shared musical passion
+      //   </p>
+      // </div>
+      // <Button signUp />
+      // <Button logIn />
 
->>>>>>> Add user controller, and css
-//       // Return this only if there IS a "this.props.current_user"
-//       // {this.renderProfileEditForm()}
-//     )
-//   }
-<<<<<<< 0be1cb319e0478416a99d25d025e5651aba168b7
-//
-=======
+      //
 
->>>>>>> Add user controller, and css
-// }
+      // Return this only if there IS a "this.props.current_user"
+      // {this.renderProfileEditForm()}
+    )
+  }
+
+
+}
