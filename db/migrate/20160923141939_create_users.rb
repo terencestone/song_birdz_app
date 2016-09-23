1d44a3a8ae2b45e8864e397f043d8fc3ba90e060
Terence Stone <email": null>
2016-09-23 10:33:12 -0400

    Create migrations

---
+++ db/migrate/20160923141939_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :provider, null: false
+      t.string :uid, null: false
+      t.string :token, null: false
+      t.datetime :age
+      t.string :gender
+      t.text :about
+      t.integer :min_age_choice
+      t.integer :max_age_choice
+
+      t.timestamps
+    end
+  end
+end
