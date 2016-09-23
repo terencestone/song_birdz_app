bd273784017dd77aa834a8bcb06a61b2b8dc33ac
Terence Stone <email": null>
2016-09-23 13:46:19 -0400

    Ignore secrets file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, '6ae2b4f168314399b3ddfda0e2016e43', 'daf76747ff524b14b3a45eda14e39e0b', scope: 'playlist-read-private user-read-private user-read-email playlist-modify-public playlist-modify-private user-library-read user-top-read user-read-birthdate'
+  provider :spotify, Rails.application.secrets.client_id, Rails.application.secrets.client_secret, scope: 'playlist-read-private user-read-private user-read-email playlist-modify-public playlist-modify-private user-library-read user-top-read user-read-birthdate'
 end
