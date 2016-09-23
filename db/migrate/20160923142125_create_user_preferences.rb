60616bfed71217a6781a491a216ec56ea16ed473
Terence Stone <email": null>
2016-09-23 11:16:47 -0400

    Create and successfully test associations

--- db/migrate/20160923142125_create_user_preferences.rb
+++ db/migrate/20160923142125_create_user_preferences.rb
@@ -1,6 +1,8 @@
 class CreateUserPreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :user_preferences do |t|
+      t.integer :user_id
+      t.integer :preference_id
 
       t.timestamps
     end
