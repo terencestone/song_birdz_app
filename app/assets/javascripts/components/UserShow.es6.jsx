ca2e4632cc7d6b2bdd84d736f836ace820f6c476
parker smathers <parkersmathers@yahoo.com>
2016-09-24 18:27:07 -0400

    Implement toggle for form

--- app/assets/javascripts/components/UserShow.es6.jsx
+++ app/assets/javascripts/components/UserShow.es6.jsx
@@ -1,9 +1,16 @@
 class UserShow extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { user: this.props.user, preferences: this.props.user.preferences }
+    this.state = {
+      user: this.props.user,
+      preferences: this.props.user.preferences,
+      formPresent: false
+    }
+
     this.updateUser = this.updateUser.bind(this)
     this.showPreferences = this.showPreferences.bind(this)
+    this.toggleForm = this.toggleForm.bind(this)
+    this.showForm = this.showForm.bind(this)
   }
 
   updateUser(response) {
@@ -20,24 +27,42 @@ class UserShow extends React.Component {
     }))
   }
 
+  toggleForm() {
+    let shouldToggle = !this.state.formPresent
+    this.setState({
+      formPresent: shouldToggle
+    })
+    // debugger
+    if (shouldToggle) {
+      return this.showForm()
+    }
+  }
+
+  showForm() {
+    // debugger
+    if (this.state.formPresent) {
+      return(<ProfileEditForm onUpdate={this.updateUser} user={this.state.user}/>)
+    } else {
+      return null
+    }
+  }
+
 
   render() {
     let user = this.state.user
 //           <pre><code>
 //         {JSON.stringify(this.props, null, 4)}
 // </code></pre>
-    return(
-      <div className="profile-container">
-        <div className="profile">
-          <h2>{user.name}</h2>
-          <p>{user.age} years old</p>
-          <p>{user.gender}</p>
-          <p>Looking for:</p>
-          <p>Ages {user.min_age_choice} to {user.max_age_choice}</p>
-          {this.showPreferences()}
-          <p>{user.about}</p>
-        </div>
-        <ProfileEditForm onUpdate={this.updateUser} user={this.state.user}/>
+      <div>
+        <h2>{user.name}</h2>
+        <p>{user.age} years old</p>
+        <p>{user.gender}</p>
+        <p>Looking for:</p>
+        <p>Ages {user.min_age_choice} to {user.max_age_choice}</p>
+        {this.showPreferences()}
+        <p>{user.about}</p>
+        <button onClick={this.toggleForm}>Edit</button>
+        {this.showForm()}
       </div>
     )
   }
