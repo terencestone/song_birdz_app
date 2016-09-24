554ab954b8c56e044b7364e82f94c4ac7cd42579
Terence Stone <email": null>
2016-09-24 14:59:16 -0400

    Ajax user edit form submit

--- app/assets/javascripts/components/ProfileEditForm.es6.jsx
+++ app/assets/javascripts/components/ProfileEditForm.es6.jsx
@@ -9,8 +9,22 @@ class ProfileEditForm extends React.Component {
 
   }
 
-  handleSubmit() {
-    // Handle put request to the appropriate "Users" controller route
+  handleSubmit(event) {
+    event.preventDefault()
+    $.ajax({
+        url: event.target.action,
+        method: event.target.method,
+        data: $(event.target).serialize()
+    }).done((response) => {
+        this.props.onUpdate(response)
+        this.refs.age.value = ""
+        this.refs.min.value = ""
+        this.refs.max.value = ""
+        this.refs.men.checked = false
+        this.refs.women.checked = false
+        this.refs.other.checked = false
+        this.refs.about.value = ""
+    })
   }
 
   renderOnboardingContainer() {
@@ -34,21 +48,21 @@ class ProfileEditForm extends React.Component {
           <form action={route} method="post" className="edit-form" onSubmit={this.handleSubmit}>
             <input type="hidden" name="_method" value="put"/>
             <div className="form-input">
-              <input className="age" type="number" name="age" placeholder="Your Age" />
+              <input ref="age" className="age" type="number" name="age" placeholder="Your Age" />
             </div>
 
             <div className="form-input">
               <label for="age-range">Age Range:</label><br/>
-              <input className="min-max" name="minAge" type="number" min="18" max="99" placeholder="min" />
+              <input ref="min" className="min-max" name="minAge" type="number" min="18" max="99" placeholder="min" />
 
               <span id="btwn"> to </span>
 
-              <input className="min-max" name="maxAge" type="number" min="19" max="100" placeholder="max" />
+              <input ref="max" className="min-max" name="maxAge" type="number" min="19" max="100" placeholder="max" />
             </div>
 
             <div className="form-input">
               <label for="gender">Gender:</label><br/>
-              <select name="gender">
+              <select ref="gender" name="gender">
               {/* Determine all of the genders that we'll be adding here */}
               <option value="male">Male</option>
               <option value="female">Female</option>
@@ -58,16 +72,16 @@ class ProfileEditForm extends React.Component {
             <div className="form-input">
               <label>Looking for: </label><br/>
                             {/* Determine all of the "looking fors" that we'll be adding here */}
-              <input type="checkbox" id="c1" name="men" />
+              <input ref="men" type="checkbox" id="c1" name="men" value="1"/>
               <label className="check" for="c1"><span></span>Men</label>
-              <input type="checkbox" id="c2" name="women" />
-              <label className="check" for="c2"><span></span>Women</label>
-              <input type="checkbox" id="c3" name="other" />
+              <input ref="women" type="checkbox" id="c2" name="women" value="2"/>
+              <label className="check" checked="on" for="c2"><span></span>Women</label>
+              <input ref="other" type="checkbox" id="c3" name="other" value="3"/>
               <label className="check" for="c3"><span></span>Other</label>
             </div>
 
             <div className="form-input">
-              <textarea name="about" placeholder="Tell us about yourself!"></textarea>
+              <textarea ref="about" name="about" placeholder="Tell us about yourself!"></textarea>
             </div>
 
             <div className="form-input">
