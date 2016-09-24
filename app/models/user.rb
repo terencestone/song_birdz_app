c1aea715bdc743efd27bd39585add16ba5badb98
Richie Ito <email": null>
2016-09-24 17:48:14 -0400

    matching for songs in birdlist works

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,28 +14,68 @@ class User < ApplicationRecord
     end
   end
 
-  def api_playlists
-    uri = URI("https://api.spotify.com/v1/users/#{self.uid}/playlists")
-    req = Net::HTTP::Get.new(uri)
-    req.add_field("Authorization", "token #{self.token}")
+  def get_birdlist
+    url = "https://api.spotify.com/v1/users/#{self.uid}/playlists/?access_token=#{self.token}"
+    response = Net::HTTP.get_response(URI.parse(url))
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    response = http.request(req)
+    playlist_hash = JSON.parse(response.body)
+    playlist_arr = playlist_hash["items"]
 
-    JSON.parse(response.body)
 
+    playlist_obj = nil
+    playlist_arr.each do |playlist|
+      if playlist["name"] == "Birdlist"
+        playlist_obj = playlist
+        break
+      end
+    end
+
+    playlist_obj
   end
 
-  # def match_list
-  #   returned_matches= []
-  #   matches= User.all
-  #
-  #   current_playlist= current_user.api_playlists.songbirdz
-  #
-  #   matches.each do |match|
-  #   end
-  #
-  # end
+  def get_tracks
+    id = self.get_birdlist["id"]
+    url = "https://api.spotify.com/v1/users/#{self.uid}/playlists/#{id}?access_token=#{self.token}&fields=tracks.items(track(name))"
+    response = Net::HTTP.get_response(URI.parse(url))
+    playlist_hash = JSON.parse(response.body)
+    tracks_arr = playlist_hash["tracks"]["items"]
+    tracks_arr.map! do |track_obj|
+      track_obj["track"]["name"]
+    end
+    tracks_arr
+  end
+
+  def preference_match
+    matches= nil
+    potential_matches = User.where.not(id: self.id)
+    self.preferences.each do |pref|
+      if pref.looking_for == "women"
+        matches = (potential_matches.where(gender: "female"))
+      elsif pref.looking_for == "men"
+        matches = (potential_matches.where(gender: "male"))
+      end
+    end
+    matches
+  end
+
+  def match_list
+    returned_matches= []
+
+    pref_matches= self.preference_match
+
+    my_tracks = self.get_tracks
+
+    my_tracks.each_with_index do |mytrack,index|
+      pref_matches.each do |match|
+        # binding.pry
+        match.get_tracks.each do |track|
+          if mytrack == track
+            returned_matches << match
+          end
+        end
+      end
+    end
+    returned_matches
+  end
 
 end
