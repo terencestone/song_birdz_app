f34866dcd23dd2494ef8fc1d8ca01cfd3b4ac4fa
Kiren Srinivasan <email": null>
2016-09-25 14:58:17 -0400

    Configures Birdlist component after it mounts

--- app/assets/javascripts/components/Birdlist.es6.jsx
+++ app/assets/javascripts/components/Birdlist.es6.jsx
@@ -2,22 +2,54 @@ class Birdlist extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: []
+      searchResults: [],
+      userPlaylists: [],
+      playlistID: ""
     };
+    this.currentBirdlist = this.currentBirdlist.bind(this);
   }
 
   componentDidMount() {
+    let userID = this.props.currentUser.uid;
+    let userToken = this.props.currentUser.token;
 
+    $.ajax({
+      url: `https://api.spotify.com/v1/users/${userID}/playlists`,
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${userToken}`,
+      }
+    })
+    .done((response) => {
+      this.setState({userPlaylists: response.items})
+      for (var i = 0; i < this.state.userPlaylists.length; i++) {
+        if (this.state.userPlaylists[i].name === "Birdlist") {
+          this.setState({playlistID: this.state.userPlaylists[i].id })
+        }
+      }
+    })
+  }
+
+  currentBirdlist() {
+    let currentBirdlist
+    if (this.state.playlistID !== "") {
+      currentBirdlist = <CurrentBirdlist currentUser={this.props.currentUser}
+                                         playlistID={this.state.playlistID} />
+    } else {
+      currentBirdlist = null
+    }
+    return currentBirdlist
   }
 
   render() {
-    <div>
-      <CurrentBirdlist currentUser={this.props.currentUser}
-                       playlistID={this.props.playlistID} />
-      {/* <BirdlistSearchBar />
+    return(
+      <div>
+        {this.currentBirdlist()}
+        {/* <BirdlistSearchBar />
 
-      // Appears after searching for a song in <BirdlistSearchBar />
-      <BirdlistSearchResults /> */}
-    </div>
+          // Appears after searching for a song in <BirdlistSearchBar />
+          <BirdlistSearchResults /> */}
+      </div>
+    )
   }
 }
