d9398cc4841a0e14c97bedbd38f70db294d253bf
Terence Stone <email": null>
2016-09-25 20:56:01 -0400

    Build basic logic for liking and crating/updating pairs

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,4 +1,8 @@
 class Pair < ApplicationRecord
   belongs_to :sender, class_name: "User"
   has_one :receiver, class_name: "User"
+
+  # validates :receiver, presence: true
+  validates :sender, uniqueness: {scope: :receiver_id}
+
 end
